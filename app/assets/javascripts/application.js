bc16c1c4ed020f46ae64ad051b5fd1a0e5eb9ea3
Joe Reiff <jcreiff@gmail.com>
2015-06-23 09:14:53 -0400

    Tuesday 6/23 challenge

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function disabledButton(){
+  var button = document.getElementById("submit-button");
+  setTimeout(function(){ button.disabled = true; }, 1);
+};
